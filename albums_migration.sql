b800ee1da0182b719e2769303154df79169f4437
JustinSOrtega <justinsortega@gmail.com>
2019-08-13 15:28:08 -0500

    Tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(150) NOT NULL,
+    name VARCHAR(150) NOT NULL,
+    release_date YEAR(4),
+    sales FLOAT,
+    genre VARCHAR(150),
+    PRIMARY KEY (id)
+);
