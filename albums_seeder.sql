224c07d163229cc38bc03af6da5a94cf611d2b53
JustinSOrtega <justinsortega@gmail.com>
2019-08-14 10:22:10 -0500

    Starting Basic Statements

---
+++ albums_seeder.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+
+INSERT INTO quotes (id, artist, name, release_date, sales, genre)
+VALUES ('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
