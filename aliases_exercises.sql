61cb7553a71c2e7d73c340ad531043bca8e791a0
JustinSOrtega <justinsortega@gmail.com>
2019-08-15 16:21:15 -0500

    EoD Thur

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,6 @@
 use employees;
 
-select concat(first_name, ' ', last_name) as full_name, birth_date as DOB
+select concat(emp_no, ' - ', first_name, ', ', last_name) as full_name, birth_date as DOB
 from employees
 limit 10;
+
