09c45f1dfc8102d54d401a7855ea27c83a77fbe9
JustinSOrtega <justinsortega@gmail.com>
2019-08-15 11:17:01 -0500

    limit done

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,9 @@ select DISTINCT last_name
 from employees
 order by last_name desc
 limit 10;
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+WHERE birth_date like ('%12-25') and hire_date like ('199%')
+order by birth_date Asc, hire_date desc
+limit 5 offset 45;
