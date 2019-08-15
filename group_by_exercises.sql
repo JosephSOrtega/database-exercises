a1f2249ad26586a99f072a56e4bda59338518106
JustinSOrtega <justinsortega@gmail.com>
2019-08-15 15:55:48 -0500

    group by done

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,3 +12,14 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name like ('e%') and last_name like ('%e')
 group by first_name, last_name;
+
+
+SELECT last_name, count(*)
+FROM employees
+WHERE last_name not like ('%qu%') and last_name like ('%q%')
+group by last_name;
+
+SELECT count(*), gender
+FROM employees
+WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+group by gender;
