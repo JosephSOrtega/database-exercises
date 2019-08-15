5ad89688ceaa73b3cc524e14022c743ff3037689
JustinSOrtega <justinsortega@gmail.com>
2019-08-15 09:35:08 -0500

    where part 2.5

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,7 +6,15 @@ WHERE gender = 'm' and (first_name = 'Irena' or first_name = 'Vidya' or first_na
 
 SELECT emp_no, first_name, last_name
 FROM employees
+WHERE last_name like ('%e%');
+
+SELECT emp_no, first_name, last_name
+FROM employees
 WHERE last_name like ('e%') or last_name like ('%e');
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name like ('e%') and last_name like ('%e');
 
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
@@ -15,6 +23,10 @@ WHERE hire_date like ('199%');
 SELECT emp_no, first_name, last_name, birth_date
 FROM employees
 WHERE birth_date like ('%12-25');
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+WHERE birth_date like ('%12-25') and hire_date like ('199%');;
 
 SELECT emp_no, first_name, last_name
 FROM employees
