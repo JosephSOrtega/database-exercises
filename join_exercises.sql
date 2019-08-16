859a449b29da455c2c4dfbf839f1de5dc37db89c
JustinSOrtega <justinsortega@gmail.com>
2019-08-16 11:47:05 -0500

    num 5 Joins

--- join_exercises.sql
+++ join_exercises.sql
@@ -5,8 +5,25 @@ FROM employees as e
          JOIN dept_manager de on e.emp_no = de.emp_no
          join departments ap on de.dept_no = ap.dept_no
 where de.to_date like '9999-%'
-group by ap.dept_name, concat(e.first_name, ' ', e.last_name) asc ;
+group by ap.dept_name, concat(e.first_name, ' ', e.last_name) asc;
 
+
+SELECT ap.dept_name as 'Department Name', concat(e.first_name, ' ', e.last_name) as 'Department Manager'
+FROM employees as e
+         JOIN dept_manager de on e.emp_no = de.emp_no
+         join departments ap on de.dept_no = ap.dept_no
+where de.to_date like '9999-%'
+  and e.gender like 'f'
+group by ap.dept_name, concat(e.first_name, ' ', e.last_name) asc;
+
+
+SELECT distinct t.title as 'Title', count(*) as 'Count'
+FROM employees as e
+         JOIN dept_emp de on e.emp_no = de.emp_no
+         join departments ap on de.dept_no = ap.dept_no
+         join titles t on e.emp_no = t.emp_no
+where ap.dept_name like 'Customer Service' and de.to_date like '9999-%'
+group by t.title asc;
 
 
 
