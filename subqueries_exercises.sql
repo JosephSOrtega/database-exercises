e9ddbe377fbc5bfab94a05b310ca4a1fb9a156c2
JustinSOrtega <justinsortega@gmail.com>
2019-08-16 16:08:43 -0500

    working on num 2, doneish

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -19,3 +19,27 @@ select t.title, e.first_name
             where first_name = 'Aamod'
         );
 
+select first_name, last_name
+from employees e
+join dept_manager de on e.emp_no = de.emp_no
+where first_name in (
+    select first_name
+    from employees
+    )
+and gender = 'f'
+and de.to_date in (
+    select to_date
+    from dept_manager
+    where de.to_date > curdate()
+    );
+# select first_name, last_name
+# from dept_manager
+# where emp_no in (
+#     select emp_no
+#     from dept_manager
+# )
+#   and gender = 'f'
+#   and to_date in (
+#     select to_date
+#     from dept_manager
+# );
