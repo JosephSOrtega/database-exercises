e5d87ffe07c54f0f0463e3f55797c48adfc88154
JustinSOrtega <justinsortega@gmail.com>
2019-08-16 09:28:34 -0500

    indexes complete

--- indexes_exercises.sql
+++ indexes_exercises.sql
@@ -12,3 +12,14 @@ select ('describe titles;');
 describe titles;
 select ('describe departments;');
 describe departments;
+
+
+use codeup_test_db;
+
+alter table albums
+add unique (name, artist);
+
+describe albums;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Micheal Jackson', 'Thriller', '1982', '47.3', 'Pop, Rock, R&B'), ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard Rock');
